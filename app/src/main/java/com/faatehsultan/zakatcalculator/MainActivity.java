fd0432b220a8a6ce78833b98d9266aaeb4b20f79
Syed Faateh Sultan Kazmi <54814772+faatehsultan@users.noreply.github.com>
2021-11-03 00:07:50 +0500

    added landscape orientation, textbox retention pending

--- app/src/main/java/com/faatehsultan/zakatcalculator/MainActivity.java
+++ app/src/main/java/com/faatehsultan/zakatcalculator/MainActivity.java
@@ -2,6 +2,8 @@ package com.faatehsultan.zakatcalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,7 +12,11 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 public class MainActivity extends AppCompatActivity {
+    public static double threshold = 87.48 * 9990; // gold price
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +34,27 @@ public class MainActivity extends AppCompatActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        zakatAmount.setText("PKR " + String.valueOf(
-                                (Integer.parseInt(cashInHand.getText().toString()) +
-                                        Integer.parseInt(investmentsAndSavings.getText().toString()) -
-                                        Integer.parseInt(loans.getText().toString()) +
-                                        Integer.parseInt(goldAndSilver.getText().toString()))*2.5/100
-                        ));
+                        int totalAmount = Integer.parseInt(cashInHand.getText().toString()) +
+                                Integer.parseInt(investmentsAndSavings.getText().toString()) -
+                                Integer.parseInt(loans.getText().toString()) +
+                                Integer.parseInt(goldAndSilver.getText().toString());
+                        if(totalAmount > threshold) {
+                            zakatAmount.setText("PKR " + String.valueOf(totalAmount*2.5/100));
+                        } else {
+                            zakatAmount.setText("PKR " + String.valueOf(0));
+                        }
+                    }
+                }
+        );
+
+
+        findViewById(R.id.callTCF).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent callTcfIntent = new Intent(Intent.ACTION_DIAL);
+                        callTcfIntent.setData(Uri.parse("tel:080000823"));
+                        startActivity(callTcfIntent);
                     }
                 }
         );
